1d89503979c695a298fb78c5bcc9c05c07ea495f
darshanaslp <darshanaslp@hotmail.com>
2020-03-12 13:40:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Angular-Iconic 5 Movie Search App
+
+## Installation
+
+In the path/newsapp>npm install
+
+Fist need to add global dependency in node npm install -g ionic
+add ionic lab npm i @ionic/lab
+
+## Run application
+
+the path/>ionic lab
+
+port will open http://localhost:8200 for android and ios view
+
+and for web port http://localhost:8100
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,20 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  // { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'movies',
+    loadChildren: () => import('./pages/movies/movies.module').then( m => m.MoviesPageModule)
+  },
+  {
+    path: 'movies/:id',
+    loadChildren: () => import('./pages/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  // {
+  //   path: 'movie-details',
+  //   loadChildren: () => import('./pages/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  // },
 ];
 
 @NgModule({
